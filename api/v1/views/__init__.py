32f47032e802cdf2899ad2945981347921e33364
KayongoYongo <sknsammie10@gmail.com>
2024-04-03 02:46:34 +0300

    added the views for orders and payments

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,3 +5,5 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.customer import *
 from api.v1.views.medicine import *
 from api.v1.views.inventory import *
+from api.v1.views.order import *
+from api.v1.views.payment import *
